aac39704b6c4436b9bd6871ba08501b155b2af72
dixonnixon <drenb@ukr.net>
2021-07-29 19:35:07 +0300

    css different global styles added

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+});
+
 export default () => {
-  return <div>
-    <StylesProvider>
+  return (<div>
+    <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/pricing" component={Pricing}></Route>
@@ -15,5 +19,5 @@ export default () => {
         </Switch>
       </BrowserRouter>
     </StylesProvider>
-  </div>
+  </div>)
 };
